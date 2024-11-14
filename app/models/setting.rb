PhantomRwcwride2Zero/consuldemocracy

Add support for third party cookies
Consul Democracy does not use third party cookies, but as most of the
forks do we are providing a way to do it.

When administradors add third party entries to this setting its content
will be shown in the cookies setup page so users can know what tools the
application runs and decide whether to use them or not.

A lot of European institutions are encouraged to accomplish the GDPR
regulations for their public sites.

app/models/setting.rb
@@ -182,7 +182,8 @@ class Setting < ApplicationRecord
         "sdg.process.polls": true,
         "sdg.process.budgets": true,
         "sdg.process.legislation": true,
-        "cookies_consent.more_info_link": ""
+        "cookies_consent.more_info_link": "",
+        "cookies_consent.third_party": false
       }
     end
 

UNDP-Somalia/bossaso

app/models/setting.rb
@@ -184,7 +184,8 @@ class Setting < ApplicationRecord
         "sdg.process.polls": true,
         "sdg.process.budgets": true,
         "sdg.process.legislation": true,
-        "cookies_consent.more_info_link": ""
+        "cookies_consent.more_info_link": "",
+        "cookies_consent.setup_page": false
       }
     end
 