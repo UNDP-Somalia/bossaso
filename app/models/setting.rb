PhantomRwcwride2Zero/consuldemocracy

[EXP] Allow administrators to force a new cookies consent ack
When the administrator changes the version name all previous
acks will be ignored and all users will be requested to accept the new
cookies policy version.

app/models/setting.rb
@@ -183,7 +183,8 @@ class Setting < ApplicationRecord
         "sdg.process.budgets": true,
         "sdg.process.legislation": true,
         "cookies_consent.more_info_link": "",
-        "cookies_consent.third_party": false
+        "cookies_consent.third_party": false,
+        "cookies_consent.version_name": "v1"
       }
     end
 

UNDP-Somalia/bossaso

app/models/setting.rb
@@ -185,7 +185,8 @@ class Setting < ApplicationRecord
         "sdg.process.budgets": true,
         "sdg.process.legislation": true,
         "cookies_consent.more_info_link": "",
-        "cookies_consent.setup_page": false
+        "cookies_consent.setup_page": false,
+        "cookies_consent.version_name": "v1"
       }
     end
 